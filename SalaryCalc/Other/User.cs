2f3ccf3971b6cdd79395b2e96ee62950b78fe7ba
Michail <bashkirovmd@gmail.com>
2021-06-10 16:07:47 +0300

    added User class

---
+++ SalaryCalc/Other/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryCalc.Other
+{
+    class User
+    {
+        public enum Permission
+        {
+            Superuser,
+            Other
+        }
+        private Permission permission;
+        private int id;
+    }
+}
