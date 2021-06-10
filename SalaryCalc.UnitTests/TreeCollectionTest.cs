1f17ddbf4c653e99a10c8c113460e25657aa8c4d
Michail <bashkirovmd@gmail.com>
2021-06-10 20:43:31 +0300

    Tree collection added

---
+++ SalaryCalc.UnitTests/TreeCollectionTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalaryCalc.Other;
+
+namespace SalaryCalc.UnitTests
+{
+    [TestClass]
+    public class TreeCollectionTest
+    {
+        TreeCollection<int> tree;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            tree = new TreeCollection<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j == 0)
+                        tree.Add(i);
+                    else
+                        tree.Add(i + 10 * j, i);
+                }
+            }
+        }
+        [TestMethod]
+        public void FindChildren_Root_Array()
+        {
+            int[] expected = {10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            int[] res = tree.FindChildren(0).ToArray();
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], res[i]);
+            }
+        }
+        [TestMethod]
+        public void FindChildren_One_Array()
+        {
+            int[] expected = { 11, 21, 31, 41, 51, 61, 71, 81, 91};
+
+            int[] res = tree.FindChildren(1).ToArray();
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], res[i]);
+            }
+        }
+    }
+}
---
+++ SalaryCalc/Other/TreeCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryCalc.Other
+{
+    public class TreeCollection<T>
+    {
+        private Node<T> root;
+
+        public bool Add(T item)
+        {
+            var newNode = new Node<T>(item); 
+            if(root == null)
+            {
+                root = newNode;
+            } 
+            else
+            {
+                root.Children.Add(newNode);
+            }
+            return true;
+        }
+        public bool Add(T item, T parent)
+        {
+            var parentNode =  FindNode(root, parent);
+            if (parentNode == null)
+                return false;
+            parentNode.Children.Add(new Node<T>(item));
+            return true;
+        }
+
+        public List<T> FindChildren(T item)
+        {
+            Node<T> parent = FindNode(root, item);
+            if (parent == null)
+                return null;
+            return parent.Children.ConvertAll(new Converter<Node<T>, T>(ExtractItem));
+        }
+
+        public T getRoot()
+        {
+            return root.Item;
+        }
+
+        private T ExtractItem(Node<T> node)
+        {
+            return node.Item;
+        }
+
+        private Node<T> FindNode(Node<T> current, T item)
+        {
+            if (item.Equals(current.Item))
+                return current;
+            foreach(Node<T> node in current.Children)
+                if (FindNode(node, item) != null)
+                    return node;
+            return null;
+        }
+
+        private class Node<A>
+        {
+            private readonly List<Node<A>> children;
+            private A item;
+
+            public Node(A item)
+            {
+                this.Item = item;
+                children = new List<Node<A>>();
+            }
+
+            public A Item { get => item; set => item = value; }
+            public List<Node<A>> Children { get => children;}
+        }
+    }
+}
